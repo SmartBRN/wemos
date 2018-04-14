086c0bed64c1c191c7e72d460ae3eeefb704a0c3
dalinar <sersh008@gmail.com>
2018-04-14 07:40:26 +0300

    Added gas detector

--- WeMos.ino/WeMos.ino.ino
+++ WeMos.ino/WeMos.ino.ino
@@ -1,4 +1,5 @@
 #define MEASURE_DHT
+#define MEASURE_GAS
 #define ENABLE_SERVER
 //#define REGISTER_DEVICE
 
@@ -19,22 +20,30 @@
 #include <DHT.h>
 #endif
 
+#define MEASURE_INTERVAL 5000
+
 #ifdef MEASURE_DHT
 #define DHTTYPE DHT22
 #define PIN_DHT D7
-#define DHT_TICKER_INTERVAL 5.0
-#define DHT_INTERVAL 5000
 #define URL_TEMPERATURE "/temperature"
 #define URL_HUMIDITY "/humidity"
+#define URL_JSON_DHT "/json/dht"
 DHT dht(PIN_DHT, DHTTYPE);
-float humidity;
-float temperature;
+float humidity = 0.0;
+float temperature = 0.0;
+#endif
+
+#ifdef MEASURE_GAS
+#define URL_GAS_DETECTOR "/gas"
+#define URL_JSON_GAS "/json/gas"
+#define PIN_FC22 D2
+bool gasDetected = false;
 #endif
 
 const char* ssid = "DIR-620";
 const char* password = "76543210";
 
-const String availableSensors[] = { "temperature", "humidity" };
+const String availableSensors[] = { "temperature", "humidity", "gas" };
 const String availableControls[] = { "relay", "relay" };
 const int numberOfSensors = 2;
 const int numberOfControls = 2;
@@ -42,8 +51,13 @@ const int numberOfControls = 2;
 #define URL_SWITCH_RELAY  "/relay"
 #define PIN_RELAY_0 D5
 #define PIN_RELAY_1 D6
+#define RELAY_0_ALIAS "relay 0"
+#define RELAY_1_ALIAS "relay 1"
+#define URL_JSON_RELAY "/json/relay"
+const int numberOfRelays = 2;
 bool relayState[] = { false, false };
 int relayPin[] = { PIN_RELAY_0, PIN_RELAY_1 };
+String relayAlias[] = { RELAY_0_ALIAS, RELAY_1_ALIAS };
 
 #ifdef ENABLE_SERVER
 ESP8266WebServer server(80);
@@ -52,6 +66,9 @@ ESP8266WebServer server(80);
 void setupPins() {
   pinMode(PIN_RELAY_0, OUTPUT);
   pinMode(PIN_RELAY_1, OUTPUT);
+  #ifdef MEASURE_GAS
+  pinMode(PIN_FC22, INPUT);
+  #endif
 }
 
 void setupWiFi() {
@@ -77,13 +94,18 @@ void setupServer() {
   server.onNotFound([](){ server.send(404, "text/plain", "404 Not Found"); });
   server.on(URL_SWITCH_RELAY, [](){
     int relayID = server.arg("id").toInt();
-    if( relayID < 0 || relayID > 1 ) {
+    String state = server.arg("state");
+    if( relayID < 0 || relayID > 1 || !(state == "on" || state == "off") ) {
       server.send(404, "text/plain", "404 Not Found"); 
     } else {
-      relayState[relayID] = !relayState[relayID];
+      relayState[relayID] = (state == "on");
       digitalWrite(relayPin[relayID], (relayState[relayID] ? HIGH : LOW));
       server.send(200, "text/plain", "200 OK");
     }    
+  });
+  server.on(URL_JSON_RELAY, [](){
+    int id = server.arg("id").toInt();
+    server.send(200, "application/json", relayStateAsJson(id));
   });
   #ifdef MEASURE_DHT
   server.on(URL_TEMPERATURE, [](){
@@ -91,6 +113,17 @@ void setupServer() {
   });
   server.on(URL_HUMIDITY, [](){
     server.send(200, "text/plain", String(humidity) + " %");
+  });
+  server.on(URL_JSON_DHT, [](){
+    server.send(200, "application/json", dhtMeasurementAsJson());
+  });
+  #endif
+  #ifdef MEASURE_GAS
+  server.on(URL_GAS_DETECTOR, [](){
+    server.send(200, "text/plain", (gasDetected ? "Gas detected: yes" : "Gas detected: no"));
+  });
+  server.on(URL_JSON_GAS, [](){
+    server.send(200, "application/json", gasMeasurementAsJson());
   });
   #endif
   
@@ -100,6 +133,18 @@ void setupServer() {
   Serial.println("/");  
 }
 #endif
+
+String relayStateAsJson(int& id) {
+  if( id < 0 || id > numberOfRelays ) return "";
+  String json;
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject(); 
+  root["id"] = id;
+  root["alias"] = relayAlias[id];
+  root["state"] = relayState[id] ? "on" : "off";
+  root.printTo(json);
+  return json;
+}
 
 #ifdef REGISTER_DEVICE
 void registerDevice() {
@@ -158,21 +203,48 @@ void setup() {
 void measureDht() {
   temperature = dht.readTemperature();
   humidity = dht.readHumidity();
+}
+
+String dhtMeasurementAsJson() {
+  String json;
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& root = jsonBuffer.createObject();
   root["sensor"] = "dht";
   root["status"] = (isnan(temperature) || isnan(humidity) ? "failed" : "success");
   root["temperature"] = temperature;
   root["humidity"] = humidity;
-  root.prettyPrintTo(Serial);
+  root.printTo(json);
+  return json;
 }
 #endif
 
-void loop() {
-  #ifdef MEASURE_DHT
-  if( millis() % DHT_INTERVAL == 0 ) {
+#ifdef MEASURE_GAS
+void measureGas() {
+  gasDetected = digitalRead(PIN_FC22) == HIGH;
+}
+
+String gasMeasurementAsJson() {
+  String json;
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["sensor"] = "gas";
+  root["status"] = "success";
+  root["gas"] = gasDetected;
+  root.printTo(json);
+  return json;
+}
+#endif
+
+void loop() {  
+  if( millis() % MEASURE_INTERVAL == 0 ) {
+    #ifdef MEASURE_DHT
     measureDht();
+    #endif
+
+    #ifdef MEASURE_GAS
+    measureGas();
+    #endif
   }
-  #endif
+  
   server.handleClient();
 }

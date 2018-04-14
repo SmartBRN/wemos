cae43e69223b861d89d687d5ff0fab940f4339c1
dalinar <sersh008@gmail.com>
2018-04-14 05:46:47 +0300

    Added humidity/temperature to server routes

--- WeMos.ino/WeMos.ino.ino
+++ WeMos.ino/WeMos.ino.ino
@@ -1,8 +1,12 @@
-//#define MEASURE_DHT
+#define MEASURE_DHT
+#define ENABLE_SERVER
 
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
+
+#ifdef ENABLE_SERVER
 #include <ESP8266WebServer.h>
+#endif
 
 #ifdef MEASURE_DHT
 #include <DHT.h>
@@ -13,6 +17,8 @@
 #define PIN_DHT D7
 #define DHT_TICKER_INTERVAL 5.0
 #define DHT_INTERVAL 5000
+#define URL_TEMPERATURE "/temperature"
+#define URL_HUMIDITY "/humidity"
 DHT dht(PIN_DHT, DHTTYPE);
 float humidity;
 float temperature;
@@ -22,18 +28,16 @@ const char* ssid = "DIR-620";
 const char* password = "76543210";
 
 #define URL_SWITCH_RELAY  "/relay"
-#define PIN_RELAY_0 D4
-#define PIN_RELAY_1 D5
-#define PIN_RELAY_2 D6
-bool relayState[] = { false, false, false };
-int relayPin[] = { PIN_RELAY_0, PIN_RELAY_1, PIN_RELAY_2 };
+#define PIN_RELAY_0 D5
+#define PIN_RELAY_1 D6
+bool relayState[] = { false, false };
+int relayPin[] = { PIN_RELAY_0, PIN_RELAY_1 };
 
 ESP8266WebServer server(80);
 
 void setupPins() {
   pinMode(PIN_RELAY_0, OUTPUT);
   pinMode(PIN_RELAY_1, OUTPUT);
-  pinMode(PIN_RELAY_2, OUTPUT);
 }
 
 void setupWiFi() {
@@ -54,11 +58,12 @@ void setupWiFi() {
   Serial.println(WiFi.localIP());
 }
 
-void setupWiFiServer() {
+#ifdef ENABLE_SERVER
+void setupServer() {
   server.onNotFound([](){ server.send(404, "text/plain", "404 Not Found"); });
   server.on(URL_SWITCH_RELAY, [](){
     int relayID = server.arg("id").toInt();
-    if( relayID < 0 || relayID > 2 ) {
+    if( relayID < 0 || relayID > 1 ) {
       server.send(404, "text/plain", "404 Not Found"); 
     } else {
       relayState[relayID] = !relayState[relayID];
@@ -66,12 +71,21 @@ void setupWiFiServer() {
       server.send(200, "text/plain", "200 OK");
     }    
   });
+  #ifdef MEASURE_DHT
+  server.on(URL_TEMPERATURE, [](){
+    server.send(200, "text/plain", String(temperature) + " *C");
+  });
+  server.on(URL_HUMIDITY, [](){
+    server.send(200, "text/plain", String(humidity) + " %");
+  });
+  #endif
   
   server.begin();
   Serial.print("Server is listening at: http://");
   Serial.print(WiFi.localIP());
   Serial.println("/");  
 }
+#endif
 
 void setup() {
   Serial.begin(9600);
@@ -81,7 +95,11 @@ void setup() {
   Serial.println("Serial port is ready.");
 
   setupWiFi();
-  setupWiFiServer();
+  
+  #ifdef ENABLE_SERVER
+  setupServer();
+  #endif
+  
   setupPins();
 
   #ifdef MEASURE_DHT
